b353bc26c33ffb13ad6454e937b1d8df5cdccf0e
claudiuri <claudio.y07@gmail.com>
2020-06-08 19:32:51 -0300

    Base do projeto

---
+++ src/controllers/rulesController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+class RulesController {
+  async create(req: Request, resp: Response){
+
+  }
+
+  async list(req: Request, resp: Response){
+
+  }
+
+  async delete(req: Request, resp: Response){
+    
+  }
+}
+
+export default new RulesController();
